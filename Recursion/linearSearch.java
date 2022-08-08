a4217b71984d57de1ed0e9a50f5de9cfc8f8abbd
Bikram shahi <97696887+bikram8@users.noreply.github.com>
2022-08-08 09:58:02 +0530

    Linear Search using recursion
    Linear Search using recursion

--- Recursion/linearSearch.java
+++ Recursion/linearSearch.java
@@ -1,1 +1,14 @@
-Linear Search using recursion.
+package recursion;
+
+public class linearSearch {
+    public static void main(String[] args) {
+        int[] arr = {2,7,8,41,52,45};
+        int target = 8;
+        System.out.println(linear(arr,target,0));
+    }
+    public static boolean linear(int[] arr , int target , int index){
+        if(index == arr.length) return false;
+
+        return arr[index] == target || linear(arr,target,index+1);
+    }
+}
